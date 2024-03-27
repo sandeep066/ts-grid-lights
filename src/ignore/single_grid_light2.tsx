32413ae716ee5595218d1fb92aa9c65630a027f7
sandeep066 <12527032+sandeep066@users.noreply.github.com>
2024-03-27 12:23:24 +0530

    Update single_grid_light2.tsx
    updated using classnames and async setGrids in handleClick

--- src/ignore/single_grid_light2.tsx
+++ src/ignore/single_grid_light2.tsx
@@ -1,71 +1,68 @@
 import "./styles.css";
 import React, { useState, useEffect } from "react";
-const initialgrids = Array.from({ length: 9 }, (_, index) => ({
+import classNames from "classnames";
+
+const initialGrids = Array.from({ length: 9 }, (_, index) => ({
   id: index,
   isGreen: false,
-  isVisible: index !== 4 && index !== 5,
-  clickedOrder: 0,
+  isVisible: index === 4 || index === 5,
+  order: 0,
 }));
 
-const Gridlight = () => {
-  const [grids, setGrids] = useState(initialgrids);
+const GridLights = () => {
+  const [grids, setGrids] = useState(initialGrids);
 
   useEffect(() => {
     if (grids.filter((grid) => grid.isGreen).length === 7) {
-      const clickedGridsReversed = grids
+      const descGreenGrids = grids
         .filter((grid) => grid.isGreen)
-        .sort((a, b) => b.clickedOrder - a.clickedOrder);
+        .sort((a, b) => b.order - a.order);
 
-      clickedGridsReversed.forEach((grid, index) => {
+      descGreenGrids.forEach((grid, index) => {
         setTimeout(() => {
-          setGrids((prevGrids) =>
-            prevGrids.map((prevGrid) =>
-              prevGrid.id === grid.id
-                ? { ...prevGrid, isGreen: false }
-                : prevGrid
-            )
-          );
+          setGrids((currentGrids) => {
+            const newGrids = [...currentGrids];
+            newGrids[grid.id] = {
+              ...newGrids[grid.id],
+              isGreen: false,
+              order: 0,
+            };
+            return newGrids;
+          });
         }, index * 1000);
       });
     }
   }, [grids]);
 
-  const handleClick = (index: number) => {
-    const newGrids = [...grids];
-    newGrids[index] = {
-      ...newGrids[index],
-      isGreen: true,
-      clickedOrder: Date.now(),
-    };
-
-    console.log(newGrids[index]);
-    setGrids(newGrids);
+  const handleClick = (id) => {
+    setGrids((currentGrids) => {
+      const newGrids = [...currentGrids];
+      newGrids[id] = { ...newGrids[id], isGreen: true, order: Date.now() };
+      return newGrids;
+    });
   };
+
   return (
-    <>
-      <h1>Hello Gridlight</h1>
-      <div className="grid-container">
-        {grids.map((grid) => {
-          return (
-            <span
-              key={grid.id}
-              className={`grid-item ${
-                grid.id === 4 || grid.id === 5 ? "hide" : null
-              } ${grid.isGreen ? "green" : ""}`}
-              onClick={() => handleClick(grid.id)}
-            >
-              {grid.id}
-            </span>
-          );
-        })}
-      </div>
-    </>
+    <div className="grid-container">
+      {grids.map((grid, index) => {
+        const gridItemClass = classNames({
+          "grid-item": true,
+          hide: grid.id === 4 || grid.id === 5,
+          green: grid.isGreen,
+        });
+        return (
+          <span className={gridItemClass} onClick={() => handleClick(grid.id)}>
+            {index}
+          </span>
+        );
+      })}
+    </div>
   );
 };
 export default function App() {
   return (
     <div className="App">
-      <Gridlight />
+      <GridLights />
     </div>
   );
 }
